f728a0b0143fb757273fbd5e296741cbee0d9517
TycOnee <mksubeesh05@gmail.com>
2025-09-20 09:30:06 +0000

    fix core hook

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -200,7 +200,7 @@ static void disable_seccomp(void)
 #ifdef CONFIG_SECCOMP
 	current->seccomp.mode = 0;
 	current->seccomp.filter = NULL;
-	atomic_set(&current->seccomp.filter_count, 0);
+	// filter_count not available in kernel 5.4
 #else
 #endif
 }
